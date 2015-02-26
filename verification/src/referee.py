9bd81dd9cd868b64d81db57c338cadba6e902aff
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-26 20:46:50 +0300

    New verification

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,23 +1,21 @@
 from checkio_referee import RefereeBase
 from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.checkers import float_compare
+from checkio_referee.verifications import FloatEqualVerification
 
 import settings
 import settings_env
 from tests import TESTS
 
+FloatEqualVerification.PRECISION = 3
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
+    VERIFICATION = FloatEqualVerification
     FUNCTION_NAME = "most_difference"
     ENV_COVERCODE = {
         "python_2": py_unwrap_args,
         "python_3": py_unwrap_args,
         "javascript": None
     }
-
-
-    def result_checker(self, test_data, result):
-        return float_compare(test_data.get["answer"], result, 3)
