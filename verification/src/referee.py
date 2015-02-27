cf9e18d7ddaa9195d886b5fd9408b610e67a8607
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-27 09:54:36 +0300

    validator rrename

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,18 +1,18 @@
 from checkio_referee import RefereeBase
 from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.verifications import FloatEqualVerification
+from checkio_referee.validators import FloatEqualValidator
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualVerification.PRECISION = 3
+FloatEqualValidator.PRECISION = 3
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
-    VERIFICATION = FloatEqualVerification
+    VALIDATOR = FloatEqualValidator
     FUNCTION_NAME = "most_difference"
     ENV_COVERCODE = {
         "python_2": py_unwrap_args,
