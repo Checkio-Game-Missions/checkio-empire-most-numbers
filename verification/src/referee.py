babf8d043063b6a3c2f972e972e2fd6ed4d0766a
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 12:45:16 +0300

    add repr

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,21 +1,28 @@
 from checkio_referee import RefereeBase
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import FloatEqualValidator
+from checkio_referee import covercodes, representations, validators
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualValidator.PRECISION = 3
+Validator = validators.FloatEqualValidator
+
+Validator.PRECISION = 3
+
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
-    VALIDATOR = FloatEqualValidator
+    VALIDATOR = Validator
     FUNCTION_NAME = "most_difference"
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_2": representations.unwrap_arg_representation,
+        "python_3": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation,
     }
